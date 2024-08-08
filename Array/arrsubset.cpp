8ae8a48307ff6695a90f6ed5cd4d1369b1f711ca
lokendra pandey <lokendrapandey5278@gmail.com>
2024-08-08 15:09:05 +0530

    practice

--- Array/arrsubset.cpp
+++ Array/arrsubset.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main(){
     int arr1[5]= {1,2,3,4,5};
-    int arr2[4]= {1,2,3,4};
+    int arr2[4]= {11,33,44,55};
     for(int i =0; i<5; i++){
         for(int j = 0;j<4; j++){
             if(arr1[i]==arr2[j]){
              cout<<"yes";
              return 0;
             }
-            else cout<<"no";
+            else{
+             cout<<"no";
+             return 0;
+
+            }
         }
     }
 }   
--- Array/maximumMinimum.cpp
+++ Array/maximumMinimum.cpp
@@ -15,23 +15,38 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     vector<int> arr ={11,22,33,44,55,66,7};
+//     int n = arr.size();
+//     sort(arr.begin(), arr.end());
+//     for(int i =0; i<n; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+//     int k =1;
+
+//     cout<<arr[n-1]<<" "<<arr[k-1];
+// }
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    vector<int> arr ={11,22,33,44,55,66,7};
+    vector<int> arr = {1,2,3,4,54,5,6,67,7,8,9};
+    int k =5;
     int n = arr.size();
     sort(arr.begin(), arr.end());
-    for(int i =0; i<n; i++){
+    for(int i =0; i<arr.size(); i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
-    int k =1;
-
-    cout<<arr[n-1]<<" "<<arr[k-1];
+    cout<<arr[n-k]<<"  "<<arr[k-1];
 }
-
-
-
 
 
 
